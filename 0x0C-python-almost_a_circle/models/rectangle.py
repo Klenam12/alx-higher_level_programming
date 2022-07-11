7debef69432124b9cd6b1e40da7666dc85f33a7e
Klenam12 <klenamtsagli@gmial.com>
2022-07-11 16:54:50 -0700

    Mode

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,71 +1,64 @@
 #!/usr/bin/python3
-"""
-Contains the "Rectangle" class
-"""
-
+import unittest
+import io
+import sys
+from models.rectangle import Rectangle
 from models.base import Base
 
 
-class Rectangle(Base):
-    """A representation of a rectangle"""
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """Initializes the rectangle"""
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-        super().__init__(id)
+class TestRectCls(unittest.TestCase):
+    def test_init(self):
+        Base._Base__nb_objects = 0
+        x = Rectangle(10, 10)
+        y = Rectangle(10, 10)
+        self.assertEqual(x.id, 1)
+        self.assertEqual(y.id, 2)
 
-    @property
-    def width(self):
-        """getter od width"""
-        return self.__width
+    def test_attrs(self):
+        Base._Base__nb_objects = 0
+        x = Rectangle(10, 10, 10, 10, 15)
+        self.assertEqual(x.width, 10)
+        self.assertEqual(x.height, 10)
+        self.assertEqual(x.x, 10)
+        self.assertEqual(x.y, 10)
+        self.assertEqual(x.id, 15)
 
-    @property
-    def height(self):
-        """getter of height"""
-        return self.__height
+    def test_attrs_type_validation(self):
+        Base._Base__nb_objects = 0
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle("20", 20)
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
+            Rectangle(20, "20")
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Rectangle(20, 20, "x", 10)
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Rectangle(20, 20, 10, "y")
+        self.assertRaises(TypeError, Rectangle, float("NaN"), float("inf"))
+        self.assertRaises(TypeError, Rectangle, float("inf"), float("NaN"))
+        self.assertRaises(TypeError, Rectangle, float("NaN"), float("NaN"),
+                          float("NaN"), float("NaN"))
+        self.assertRaises(TypeError, Rectangle, float("inf"), float("inf"),
+                          float("inf"), float("inf"))
+        self.assertRaises(TypeError, Rectangle, None, None)
+        self.assertRaises(TypeError, Rectangle)
+        self.assertRaises(TypeError, Rectangle, [10, 10], [10, 10],
+                          [10, 10], [10, 10])
+        self.assertRaises(TypeError, Rectangle, "abebe", "abebe",
+                          "abebe", "abebe")
+        self.assertRaises(TypeError, Rectangle, 10, (10, 10),
+                          [10, 10], {10, 10})
 
-    @property
-    def x(self):
-        """getter of x"""
-        return self.__x
+    def test_attrs_value_validation(self):
+        Base._Base__nb_objects = 0
+        w_err = "width must be > 0"
+        self.assertRaisesRegex(ValueError, w_err, Rectangle, -20, 20)
+        self.assertRaisesRegex(ValueError, w_err, Rectangle, 0, 20)
+        self.assertRaisesRegex(ValueError, w_err, Rectangle, -20000000, 20)
 
-    @property
-    def y(self):
-        """getter of y"""
-        return self.__y
+        h_err = "height must be > 0"
+        self.assertRaisesRegex(ValueError, h_err, Rectangle, 20, -20)
+        self.assertRaisesRegex(ValueError, h_err, Rectangle, 20, 0)
+        self.assertRaisesRegex(ValueError, h_err, Rectangle, 20, -20000000)
 
-    @width.setter
-    def width(self, value):
-        """setter of width"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
-
-    @height.setter
-    def height(self, value):
-        """setter of height"""
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
-    @x.setter
-    def x(self, value):
-        """setter of x"""
-        if type(value) is not int:
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
-    @y.setter
-    def y(self, value):
-        """setter of y"""
-        if type(value) is not int:
-            raise TypeError("y must be an integer")
+        x_err = "x must be >= 0"
 
