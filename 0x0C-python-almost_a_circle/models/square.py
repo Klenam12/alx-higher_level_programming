e2b4117b4fa2f406f43fe50a68ff28cb6b8edc57
Klenam12 <klenamtsagli@gmial.com>
2022-07-11 17:21:32 -0700

    Mode

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,64 +1,59 @@
 #!/usr/bin/python3
-import unittest
-import json
-import io
-import sys
-
+"""contains Square class to represent squares"""
 from models.rectangle import Rectangle
-from models.base import Base
-from models.square import Square
 
 
-class TestSquare(unittest.TestCase):
-    def test_init(self):
-        Base._Base__nb_objects = 0
-        x = Square(10)
-        y = Square(10)
-        self.assertEqual(x.id, 1)
-        self.assertEqual(y.id, 2)
+class Square(Rectangle):
+    """class to represent a Square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Instantiation of a Square object
+        Args:
+            size (int): the size of the square
+            x (int): the x coordinate
+            y (int): the y coordinate
+            id (int): the id of the object
+        """
+        super().__init__(size, size, x, y, id)
 
-    def test_attrs(self):
-        Base._Base__nb_objects = 0
-        x = Square(10, 10, 10, 15)
-        self.assertEqual(x.size, 10)
-        self.assertEqual(x.x, 10)
-        self.assertEqual(x.y, 10)
-        self.assertEqual(x.id, 15)
-        x = Square(56444, 500, 90000, 240000)
-        self.assertEqual(x.size, 56444)
-        self.assertEqual(x.x, 500)
-        self.assertEqual(x.y, 90000)
-        self.assertEqual(x.id, 240000)
+    @property
+    def size(self):
+        """size attribute getter"""
+        return self.width
 
-    def test_attrs_type_validation(self):
-        Base._Base__nb_objects = 0
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Square("20", 20)
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Square(4.3, 20)
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Square({2: 1}, 20)
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Square("20")
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Square(9.0)
-        with self.assertRaisesRegex(TypeError, "width must be an integer"):
-            Square(["hello world", "?"])
-        with self.assertRaisesRegex(TypeError, "x must be an integer"):
-            Square(20, "x", 10)
-        with self.assertRaisesRegex(TypeError, "x must be an integer"):
-            Square(20, -42.3, 10)
-        with self.assertRaisesRegex(TypeError, "y must be an integer"):
-            Square(20, 10, "y")
-        with self.assertRaisesRegex(TypeError, "y must be an integer"):
-            Square(20, 10, 5.2)
-        self.assertRaises(TypeError, Square, float("NaN"))
-        self.assertRaises(TypeError, Square, float("inf"))
-        self.assertRaises(TypeError, Square, float("NaN"),
-                          float("NaN"), float("NaN"))
-        self.assertRaises(TypeError, Square, float("inf"),
-                          float("inf"), float("inf"))
-        self.assertRaises(TypeError, Square, None)
-        self.assertRaises(TypeError, Square)
-        self.assertRaises(TypeError, Square, [10, 10],
+    @size.setter
+    def size(self, value):
+        """size attribute setter"""
+        self.width = value
+        self.height = value
 
+    def __str__(self):
+        """formatted output"""
+        s = "[Square] ({}) {}/{} - {}".format(self.id, self.x,
+                                              self.y, self.width)
+        return s
+
+    def update(self, *args, **kwargs):
+        """updates a square with new values for attributes"""
+        attrs = ["id", "width", "x", "y"]
+        if args is not None and len(args) != 0:
+            i = 0
+            while i < len(attrs) and i < len(args):
+                setattr(self, attrs[i], args[i])
+                i += 1
+        else:
+            for attr in attrs:
+                if attr == "width" and "size" in kwargs.keys():
+                    setattr(self, attr, kwargs["size"])
+                if attr in kwargs.keys():
+                    setattr(self, attr, kwargs[attr])
+
+    def to_dictionary(self):
+        """represents a Square as a dict"""
+        rect_dict = {}
+        attrs = ["id", "size", "x", "y"]
+        for attr in attrs:
+            if attr == "size":
+                rect_dict.update({attr: getattr(self, "width")})
+            else:
+                rect_dict.update({attr: getattr(self, attr)})
+        return rect_dict
